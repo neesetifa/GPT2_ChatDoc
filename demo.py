96f52c50f7d1d6982ffa155ba98b0a084eaece72
neesetifa <36785948+neesetifa@users.noreply.github.com>
2024-12-06 15:05:20 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,24 +1,29 @@
 from transformers import GPT2Tokenizer, GPT2LMHeadModel, TextGenerationPipeline
 import pdb
 
-def main(model_path):
+def main(model_path, mode = 'doctor'):
+    assert mode in ['chat', 'doctor']
     tokenizer = GPT2Tokenizer.from_pretrained(model_path)
     # tokenizer.pad_token = tokenizer.eos_token
     model = GPT2LMHeadModel.from_pretrained(model_path)
     text_generator = TextGenerationPipeline(model, tokenizer)
     
     print('## 蛋饼🥚: Hello, I\'m Egg-Cookie, what do you want to know today?')
-    instruction = 'Below is an instruction that describes a task. Write a response that appropriately completes the request.\n\n### '
-    human = 'Instruction:\n'
-    bot = '### Response:'
+    prompt = 'Below is an instruction that describes a task. Write a response that appropriately completes the request.\n\n'
+    if mode == 'chat':
+        human_instruct = '### Instruction:\n'
+        human_input = ''  # During chitchat, human use instruction, no input is used
+    elif mode == 'doctor':
+        human_instruct = '### Instruction:\nIf you are a doctor, please answer the medical questions based on the patient\'s description.\n\n'
+        human_input = '### Input:\n'
+    bot = '### Response:\n'
+    
     history = []
-
     while True:
         human_question = input('** 饼干🍪: ')
-        # human_question = 'Give three tips for staying healthy.'
-        history.append(human+human_question)
+        history.append(human_instruct + human_input + human_question)
         
-        full_text = instruction + '\n\n'.join(history) + bot
+        full_text = prompt + '\n\n'.join(history) + bot
         result = text_generator(full_text, max_length=500, do_sample=True,
                                 pad_token_id=text_generator.tokenizer.eos_token_id)
         result_text = result[0]['generated_text']
@@ -36,8 +41,9 @@ def main(model_path):
 
 
 if __name__ == "__main__":
-    model_path = './exp/checkpoint-9700/'
+    model_path = './exp2/checkpoint-30000/'
     main(model_path)
 # Tell me something about Thanksgiving.
 # Why do we eat turkey on Thanksgiving?
 # What do people usually do after having Thanksgiving dinner?
+# I caught a cold yesterday, now I'm sneezing and coughing, what should I do?
