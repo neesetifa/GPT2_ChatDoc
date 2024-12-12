fecf4aa24071465d0be85ee83a1eab7b7ee64798
neesetifa <36785948+neesetifa@users.noreply.github.com>
2024-12-12 12:18:38 +0800

    Add files via upload

---
+++ dataset/sample_data.py
@@ -0,0 +1,11 @@
+sample_data = [
+    {"instruction": "If you are a doctor, please answer the medical questions based on the patient's description.",
+  "input": "i had what feels like a muscle cramp about an hour ago under the left bottom rib. it lasted about a minute and then went away. i had no other pains or dificulties since. could this have been a simptom of a minor heart attack. do heart attack symptoms come one at a time or are there more than one symptom when they occur?",
+  "output": "No this is not a symptom of great attack.... it is normal after some stressful activity. No need to worry. If same thing happen again let me know"
+     },
+    
+    {"instruction": "If you are a doctor, please answer the medical questions based on the patient's description.",
+     "input": "I woke up this morning feeling the whole room is spinning when i was sitting down. I went to the bathroom walking unsteadily, as i tried to focus i feel nauseous. I try to vomit but it wont come out.. After taking panadol and sleep for few hours, i still feel the same.. By the way, if i lay down or sit down, my head do not spin, only when i want to move around then i feel the whole world is spinning.. And it is normal stomach discomfort at the same time? Earlier after i relieved myself, the spinning lessen so i am not sure whether its connected or coincidences.. Thank you doc!",
+     "output": "Hi, Thank you for posting your query. The most likely cause for your symptoms is benign paroxysmal positional vertigo (BPPV), a type of peripheral vertigo. In this condition, the most common symptom is dizziness or giddiness, which is made worse with movements. Accompanying nausea and vomiting are common. The condition is due to problem in the ear, and improves in a few days on own. Betahistine tablets would help relieve your symptoms. Doing vestibular rehabilitation or adaptation exercises would prevent the recurrence of these symptoms. An ENT evaluation would also help. I hope it helps. Best wishes, Chat Doctor. "
+     },
+]
